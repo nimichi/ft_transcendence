db15f2d0aace30902be23ebaed5c38b44dd39bd8
Romy Schleicher <rschleic@1-D-21.42heilbronn.de>
2023-04-12 18:07:50 +0200

    video added

---
+++ requirements/angular_frontend/files/src/app/shared/shared.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+// import { ModalService } from '../services/modal.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideNgxMask } from 'ngx-mask';
+
+
+@NgModule({
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+
+  ],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent
+  ],
+  // providers: [
+  //   // ModalService,
+  //   provideNgxMask
+  // ]
+})
+export class SharedModule { }
