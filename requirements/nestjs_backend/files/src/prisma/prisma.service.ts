c84ff65bfb8ff2acfbb437397744ba07500f3c8d
Lukas Rosch <lrosch@3-C-1.42heilbronn.de>
2023-04-18 19:45:02 +0200

    added prisma.service to create a user in auth module

--- requirements/nestjs_backend/files/src/prisma/prisma.service.ts
+++ requirements/nestjs_backend/files/src/prisma/prisma.service.ts
@@ -1,11 +1,24 @@
 // src/prisma/prisma.service.ts
 
 import { INestApplication, Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User, Prisma } from '@prisma/client';
+import { from, Observable } from 'rxjs'
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  async enableShutdownHooks(app: INestApplication) {
-      await app.close();
-  }
+	async onModuleInit() {
+		await this.$connect();
+	  }
+
+	async enableShutdownHooks(app: INestApplication) {
+		this.$on('beforeExit', async () => {
+		  await app.close();
+		});
+	}
+
+	async createUser(data: User): Promise<User> {
+		const user = await this.user.create({ data });
+		return user;
+	  }
 }
