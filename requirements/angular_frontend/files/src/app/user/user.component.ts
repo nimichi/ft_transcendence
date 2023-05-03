92a9a89965c371b3a7962ca1dce4fb2561bc8e94
Romy Schleicher <rschleic@3-B-7.42heilbronn.de>
2023-05-03 20:44:48 +0200

    User Page concept works

--- requirements/angular_frontend/files/src/app/user/user.component.ts
+++ requirements/angular_frontend/files/src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SocketService } from '../services/socket.service';
+import { ChatComponent } from '../chat/chat.component';
 
 @Component({
   selector: 'app-user',
@@ -9,8 +10,20 @@ import { SocketService } from '../services/socket.service';
 })
 export class UserComponent {
 
+	intraPic: string  = "https://cdn.intra.42.fr/users/439ae812911986ad4e2b01a32ef73ea4/rschleic.jpg"
+	intraName: string  = "Rschleic"
+	realName: string = "Romy Schleicher"
+	wins: number = 0
+	losses: number = 0
+	level: number = 0
+	intraBadgeLevel: number = 2
+	versus: string = "mnies"
+	result:string = "WIN"
+	laddderLevel: number = 100
+	listData: any [ ] = [{versus: "romy", result: "win", level: 3},{versus: "michi", result: "loss", level: 4}]
+	//die liste kommt vom backend
+	
 	constructor(private activatedRoute: ActivatedRoute, private socket: SocketService) {
-
 	}
 
 	ngOnInit() {
@@ -25,7 +38,10 @@ export class UserComponent {
 			console.log('loaded with code: ' + params.code);
 		});
 
-		this.socket.requestUserData();
+		console.log(this.socket.requestUserData());
+		//entpacken
+		//speicher in vars
+		
 	}
 
 }
